f2ef2e178f80bd6ec0045d99e4a98591c10b6f96
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-31 09:37:24 +0200

    Lecture 24 Refactoring separate classes

---
+++ Scripts/GridStructure.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridStructure
+{
+    public int cellSize = 3;
+    public Vector3 CalculateGridPosition(Vector3 inputPosition)
+    {
+        int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
+        return new Vector3(x * cellSize, 0, z * cellSize);
+    }
+
+
+}
--- Scripts/InputManager.cs
+++ Scripts/InputManager.cs
@@ -9,7 +9,7 @@ public class InputManager : MonoBehaviour
 
     public LayerMask mouseInputMask;
     public GameObject buildingPrefab;
-    public int cellSize = 3;
+    
 
     void Update()
     {
@@ -25,23 +25,16 @@ public class InputManager : MonoBehaviour
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
             {
                 Vector3 position = hit.point-transform.position;
-                Debug.Log(CalculateGridPosition(position));
-                CreatebUilding(CalculateGridPosition(position));
 
             }
         }
         
     }
 
-    public Vector3 CalculateGridPosition(Vector3 inputPosition)
-    {
-        int x = Mathf.FloorToInt((float)inputPosition.x/cellSize);
-        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
-        return new Vector3(x * cellSize, 0, z * cellSize);
-    }
 
-    private void CreatebUilding(Vector3 gridPosition)
-    {
-        Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
-    }
+
+    //private void CreatebUilding(Vector3 gridPosition)
+    //{
+    //    Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+    //}
 }
