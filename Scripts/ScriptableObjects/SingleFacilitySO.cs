837651c2eff24574ec0efc735ef54a740f42ff1a
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-31 10:05:30 +0200

    Lecture 86 Adding functionality to structures

--- Scripts/ScriptableObjects/SingleFacilitySO.cs
+++ Scripts/ScriptableObjects/SingleFacilitySO.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,4 +8,34 @@ public class SingleFacilitySO : SingleStructureBaseSO
 {
     public int maxCustomers;
     public int upkeepPerCustomer;
+    private HashSet<StructureBaseSO> customers = new HashSet<StructureBaseSO>();
+    public FacilityType facilityType = FacilityType.None;
+
+    public void RemoveClient(StructureBaseSO clientStructure)
+    {
+        if (customers.Contains(clientStructure))
+        {
+            if (facilityType == FacilityType.Water)
+            {
+                clientStructure.RemoveWaterFacility();
+            }
+            if (facilityType == FacilityType.Power)
+            {
+                clientStructure.RemovePowerFacility();
+            }
+            customers.Remove(clientStructure);
+        }
+    }
+
+    public override int GetIncome()
+    {
+        return customers.Count * income;
+    }
+
+    public enum FacilityType
+    {
+        Power,
+        Water,
+        None
+    }
 }
--- Scripts/ScriptableObjects/StructureBaseSO.cs
+++ Scripts/ScriptableObjects/StructureBaseSO.cs
@@ -14,9 +14,88 @@ public abstract class StructureBaseSO : ScriptableObject
     public bool requireRoadAccess;
     public bool requireWater;
     public bool requirePower;
+    public int structureRange = 1;
+    private SingleFacilitySO powerProvider = null;
+    private SingleFacilitySO waterProvider = null;
+    private RoadStructureSO roadProvider = null;
+
+    public SingleFacilitySO PowerProvider { get => powerProvider;}
+    public SingleFacilitySO WaterProvider { get => waterProvider;}
+    public RoadStructureSO RoadProvider { get => roadProvider;}
 
     public virtual int GetIncome()
     {
         return income;
+    }
+
+    public bool HasPower()
+    {
+        return powerProvider != null;
+    }
+
+    public bool HasWater()
+    {
+        return powerProvider != null;
+    }
+
+    public bool HasRoadAccess()
+    {
+        return roadProvider != null;
+    }
+
+    internal void RemoveRoadProvider()
+    {
+        roadProvider = null;
+    }
+
+    public void PreareStructure(IEnumerable<StructureBaseSO> structuresInRange)
+    {
+        AddRoadProvider(structuresInRange);
+    }
+
+    public void AddPowerFacility(SingleFacilitySO facility)
+    {
+        if (powerProvider==null)
+            powerProvider = facility;
+    }
+
+    public void PrepareForDestruction()
+    {
+        if (powerProvider != null)
+            powerProvider.RemoveClient(this);
+        if (waterProvider != null)
+            waterProvider.RemoveClient(this);
+    }
+    public void AddWaterFacility(SingleFacilitySO facility)
+    {
+        if (waterProvider==null)
+            powerProvider = facility;
+    }
+
+    public void RemovePowerFacility()
+    {
+
+        powerProvider = null;
+
+
+    }
+    public void RemoveWaterFacility()
+    {
+
+        waterProvider = null;
+
+    }
+    private void AddRoadProvider(IEnumerable<StructureBaseSO> structures)
+    {
+        if (roadProvider == null)
+            return;
+        foreach (var nearbyStructure in structures)
+        {
+            if (nearbyStructure.GetType() == typeof(RoadStructureSO))
+            {
+                roadProvider = (RoadStructureSO)nearbyStructure;
+                return;
+            }
+        }
     }
 }
