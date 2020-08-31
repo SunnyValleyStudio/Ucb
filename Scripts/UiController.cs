ab96f0a5b225cde2afe88928dc391a214f0f9660
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-31 09:46:12 +0200

    Lecture 45 - Data preparing Ui

--- Scripts/UiController.cs
+++ Scripts/UiController.cs
@@ -19,34 +19,66 @@ public class UiController : MonoBehaviour
     public Button openBuildMenuBtn;
     public Button demolishBtn;
 
+    public GameObject zonesPanel;
+    public GameObject facilitiesPanel;
+    public GameObject roadsPanel;
+    public Button closeBuildMenuBtn;
+
+    public GameObject buildButtonPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         cancleActionPanel.SetActive(false);
         buildingMenuPanel.SetActive(false);
-        buildResidentialAreaBtn.onClick.AddListener(OnBuildAreaCallback);
+        //buildResidentialAreaBtn.onClick.AddListener(OnBuildAreaCallback);
         cancleActionBtn.onClick.AddListener(OnCancleActionCallback);
         openBuildMenuBtn.onClick.AddListener(OnOpenBuildMenu);
         demolishBtn.onClick.AddListener(OnDemolishHandler);
+        closeBuildMenuBtn.onClick.AddListener(OnCloseMenuHandler);
+    }
 
+    private void OnCloseMenuHandler()
+    {
+        buildingMenuPanel.SetActive(false);
     }
 
     private void OnDemolishHandler()
     {
         OnDemolishActionHandler?.Invoke();
         cancleActionPanel.SetActive(true);
-        buildingMenuPanel.SetActive(false);
+        OnCloseMenuHandler();
     }
 
     private void OnOpenBuildMenu()
     {
         buildingMenuPanel.SetActive(true);
+        PrepareBuildMenu();
+    }
+
+    private void PrepareBuildMenu()
+    {
+        CreateButtonsInPanel(zonesPanel.transform);
+        CreateButtonsInPanel(facilitiesPanel.transform);
+        CreateButtonsInPanel(roadsPanel.transform);
+    }
+
+    private void CreateButtonsInPanel(Transform panelTransform)
+    {
+        foreach (Transform child in panelTransform)
+        {
+            var button = child.GetComponent<Button>();
+            if(button!= null)
+            {
+                button.onClick.AddListener(OnBuildAreaCallback);
+            }
+        }
     }
 
     private void OnBuildAreaCallback()
     {
         cancleActionPanel.SetActive(true);
-        buildingMenuPanel.SetActive(false);
+        OnCloseMenuHandler();
         OnBuildAreaHandler?.Invoke();
     }
 
