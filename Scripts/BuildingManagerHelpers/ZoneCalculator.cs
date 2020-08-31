5e7cf1783a9429b95ba0077544abd4a9503e87de
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-31 10:00:28 +0200

    Lecture 72 Zone placement 1

---
+++ Scripts/BuildingManagerHelpers/ZoneCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ZoneCalculator
+{
+
+    public static bool CheckIfPositionHasChanged(Vector3 gridPosition, Vector3 previousPosition, GridStructure grid)
+    {
+        return Vector3Int.FloorToInt(gridPosition).Equals(Vector3Int.FloorToInt(previousPosition)) == false;
+    }
+
+}
---
+++ Scripts/BuildingManagerHelpers/ZonePlacementHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonePlacementHelper : StructureModificationHelper
+{
+    Vector3 startPosition;
+    Vector3? previousEndPositon = null;
+    bool startPositionAcquired = false;
+    Vector3 mapBottomLeftCorner;
+    Queue<GameObject> gameObjectsToReuse = new Queue<GameObject>();
+    public ZonePlacementHelper(StructureRepository structureRepository, GridStructure grid, IPlacementManager placementManager, Vector3 mapBottomLeftCorner) : base(structureRepository, grid, placementManager)
+    {
+        this.mapBottomLeftCorner = mapBottomLeftCorner;
+    }
+
+    public override void PrepareStructureForModification(Vector3 inputPosition, string structureName, StructureType structureType)
+    {
+        base.PrepareStructureForModification(inputPosition, structureName, structureType);
+        Vector3 gridPositon = grid.CalculateGridPosition(inputPosition);
+        if (startPositionAcquired == false && grid.IsCellTaken(gridPositon) == false)
+        {
+            startPosition = gridPositon;
+            startPositionAcquired = true;
+        }
+        if (startPositionAcquired && previousEndPositon == null || ZoneCalculator.CheckIfPositionHasChanged(gridPositon, previousEndPositon.Value, grid))
+        {
+            PlaceNewZoneUpTo(gridPositon);
+        }
+
+    }
+
+    private void PlaceNewZoneUpTo(Vector3 endPosition)
+    {
+        
+
+    }
+
+
+}
