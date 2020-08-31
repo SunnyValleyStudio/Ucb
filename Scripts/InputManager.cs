f15cb3a94c073ac3e102c84c74bdbdeab1d54347
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-31 09:34:48 +0200

    Lecture 23 Placing structure on out map

--- Scripts/InputManager.cs
+++ Scripts/InputManager.cs
@@ -8,6 +8,8 @@ public class InputManager : MonoBehaviour
 {
 
     public LayerMask mouseInputMask;
+    public GameObject buildingPrefab;
+    public int cellSize = 3;
 
     void Update()
     {
@@ -23,11 +25,23 @@ public class InputManager : MonoBehaviour
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
             {
                 Vector3 position = hit.point-transform.position;
-                Debug.Log(position);
+                Debug.Log(CalculateGridPosition(position));
+                CreatebUilding(CalculateGridPosition(position));
 
             }
         }
         
     }
 
+    public Vector3 CalculateGridPosition(Vector3 inputPosition)
+    {
+        int x = Mathf.FloorToInt((float)inputPosition.x/cellSize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
+        return new Vector3(x * cellSize, 0, z * cellSize);
+    }
+
+    private void CreatebUilding(Vector3 gridPosition)
+    {
+        Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+    }
 }
