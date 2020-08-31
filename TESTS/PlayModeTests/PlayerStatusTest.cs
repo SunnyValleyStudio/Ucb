0d4ce0a9c6e06ffb4d0da57ceb4fd707543ddfa4
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-31 09:48:32 +0200

    Lecture 51 Fixing playmode tests

--- TESTS/PlayModeTests/PlayerStatusTest.cs
+++ TESTS/PlayModeTests/PlayerStatusTest.cs
@@ -14,38 +14,84 @@ namespace Tests
         UiController uiController;
         GameManager gameManagerComponent;
 
+        //[SetUp]
+        //public void Init()
+        //{
+        //    GameObject gameManagerObject = new GameObject();
+        //    var camerMovementComponent = gameManagerObject.AddComponent<CameraMovement>();
+
+        //    uiController = gameManagerObject.AddComponent<UiController>();
+        //    GameObject buttonBuildObject = new GameObject();
+        //    GameObject cancleButtonObject = new GameObject();
+        //    GameObject canclePane = new GameObject();
+        //    uiController.cancleActionBtn = cancleButtonObject.AddComponent<Button>();
+        //    var buttonBuildComponent = buttonBuildObject.AddComponent<Button>();
+        //    uiController.buildResidentialAreaBtn = buttonBuildComponent;
+        //    uiController.cancleActionPanel = cancleButtonObject;
+
+        //    uiController.buildingMenuPanel = canclePane;
+        //    uiController.openBuildMenuBtn = uiController.cancleActionBtn;
+        //    uiController.demolishBtn = uiController.cancleActionBtn;
+
+        //    gameManagerComponent = gameManagerObject.AddComponent<GameManager>();
+        //    gameManagerComponent.cameraMovement = camerMovementComponent;
+        //    gameManagerComponent.uiController = uiController;
+        //}
+
         [SetUp]
         public void Init()
         {
             GameObject gameManagerObject = new GameObject();
             var camerMovementComponent = gameManagerObject.AddComponent<CameraMovement>();
-            uiController = gameManagerObject.AddComponent<UiController>();
-            GameObject buttonBuildObject = new GameObject();
-            GameObject cancleButtonObject = new GameObject();
-            GameObject canclePane = new GameObject();
-            uiController.cancleActionBtn = cancleButtonObject.AddComponent<Button>();
-            var buttonBuildComponent = buttonBuildObject.AddComponent<Button>();
-            uiController.buildResidentialAreaBtn = buttonBuildComponent;
-            uiController.cancleActionPanel = cancleButtonObject;
 
-            uiController.buildingMenuPanel = canclePane;
-            uiController.openBuildMenuBtn = uiController.cancleActionBtn;
-            uiController.demolishBtn = uiController.cancleActionBtn;
+            uiController = Substitute.For<UiController>();
 
             gameManagerComponent = gameManagerObject.AddComponent<GameManager>();
             gameManagerComponent.cameraMovement = camerMovementComponent;
             gameManagerComponent.uiController = uiController;
         }
 
-
         [UnityTest]
         public IEnumerator PlayerStatusPlayerBuildingSingleStructureStateTestWithEnumeratorPasses()
         {
             yield return new WaitForEndOfFrame(); //awake
             yield return new WaitForEndOfFrame(); //start
-            uiController.buildResidentialAreaBtn.onClick.Invoke();
+            gameManagerComponent.State.OnBuildSingleStructure(null);
             yield return new WaitForEndOfFrame();
             Assert.IsTrue(gameManagerComponent.State is PlayerBuildingSingleStructureState);
+
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerStatusPlayerBuildAreaStateTestWithEnumeratorPasses()
+        {
+            yield return new WaitForEndOfFrame(); //awake
+            yield return new WaitForEndOfFrame(); //start
+            gameManagerComponent.State.OnBuildArea(null);
+            yield return new WaitForEndOfFrame();
+            Assert.IsTrue(gameManagerComponent.State is PlayerBuildingZoneState);
+
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerStatusPlayerBuildingRoadStateTestWithEnumeratorPasses()
+        {
+            yield return new WaitForEndOfFrame(); //awake
+            yield return new WaitForEndOfFrame(); //start
+            gameManagerComponent.State.OnBuildRoad(null);
+            yield return new WaitForEndOfFrame();
+            Assert.IsTrue(gameManagerComponent.State is PlayerBuildingRoadState);
+
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerStatusPlayerRemoveBuildingStateTestWithEnumeratorPasses()
+        {
+            yield return new WaitForEndOfFrame(); //awake
+            yield return new WaitForEndOfFrame(); //start
+            gameManagerComponent.State.OnDemolishAction();
+            yield return new WaitForEndOfFrame();
+            Assert.IsTrue(gameManagerComponent.State is PlayerRemoveBuildingState);
 
         }
 
