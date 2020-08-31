60b7a6271021262203e87d30cf18cf2d3aa379ca
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-31 09:34:11 +0200

    Lecture 22 - Getting mouse input

---
+++ Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+
+    public LayerMask mouseInputMask;
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
+            {
+                Vector3 position = hit.point-transform.position;
+                Debug.Log(position);
+
+            }
+        }
+        
+    }
+
+}
