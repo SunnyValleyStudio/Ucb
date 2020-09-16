12be2be402339061edcb5ff2d0064684e63a89f0
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-09-16 09:35:28 +0200

    Lecture 98 - UIStructrureHelper

--- Scripts/UIStructureInfoPanelHelper.cs
+++ Scripts/UIStructureInfoPanelHelper.cs
@@ -24,5 +24,103 @@ public class UIStructureInfoPanelHelper : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    
+    public void DisplayBasicStructureInfo(StructureBaseSO data)
+    {
+        Show();
+        HideElement(clientsTxt.gameObject);
+        HideElement(powerToggle.gameObject);
+        HideElement(waterToggle.gameObject);
+        HideElement(roadToggle.gameObject);
+        SetText(nameTxt, data.buildingName);
+        SetText(incomeTxt, data.GetIncome() + "");
+        SetText(upkeepTxt, data.upkeepCost + "");
+    }
+
+    public void DisplayZoneStructureInfo(ZoneStructureSO data)
+    {
+        Show();
+        HideElement(clientsTxt.gameObject);
+
+        SetText(nameTxt, data.buildingName);
+        SetText(incomeTxt, data.GetIncome() + "");
+        SetText(upkeepTxt, data.upkeepCost + "");
+        if (data.requirePower)
+        {
+            SetToggle(powerToggle, data.HasPower());
+        }
+        else
+        {
+            HideElement(powerToggle.gameObject);
+        }
+        if (data.requireWater)
+        {
+            SetToggle(waterToggle, data.HasWater());
+        }
+        else
+        {
+            HideElement(waterToggle.gameObject);
+        }
+        if (data.requireRoadAccess)
+        {
+            SetToggle(roadToggle, data.HasRoadAccess());
+        }
+        else
+        {
+            HideElement(roadToggle.gameObject);
+        }
+    }
+
+    public void DisplayFacilityStructureInfo(SingleFacilitySO data)
+    {
+        Show();
+        SetText(nameTxt, data.buildingName);
+        SetText(incomeTxt, data.GetIncome() + "");
+        SetText(upkeepTxt, data.upkeepCost + "");
+        if (data.requirePower)
+        {
+            SetToggle(powerToggle, data.HasPower());
+        }
+        else
+        {
+            HideElement(powerToggle.gameObject);
+        }
+        if (data.requireWater)
+        {
+            SetToggle(waterToggle, data.HasWater());
+        }
+        else
+        {
+            HideElement(waterToggle.gameObject);
+        }
+        if (data.requireRoadAccess)
+        {
+            SetToggle(roadToggle, data.HasRoadAccess());
+        }
+        else
+        {
+            HideElement(roadToggle.gameObject);
+        }
+        SetText(clientsTxt, data.GetNumberOfCustomers() + "/" + data.maxCustomers);
+    }
+
+    private void HideElement(GameObject element)
+    {
+        element.transform.parent.gameObject.SetActive(false);
+    }
+    private void ShowElement(GameObject element)
+    {
+        element.transform.parent.gameObject.SetActive(true);
+    }
+
+    private void SetText(TextMeshProUGUI element, string value)
+    {
+        ShowElement(element.gameObject);
+        element.text = value;
+    }
+
+    private void SetToggle(Toggle element, bool value)
+    {
+        ShowElement(element.gameObject);
+        element.isOn = value;
+    }
 }
